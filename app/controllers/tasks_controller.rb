b690af6a97957c4ef4c0023d627e26663eb93338
tewodros <teddyhailu5437@gmail.com>
2019-06-30 22:31:46 +0000

    set-up

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,23 @@
 class TasksController < ApplicationController
+def index
+    render json: Task.order(:id)
+  end
 
-  def index
-    render json: Task.all
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    render json: task
+  end
+
+ 
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:done, :title)
   end
 end
